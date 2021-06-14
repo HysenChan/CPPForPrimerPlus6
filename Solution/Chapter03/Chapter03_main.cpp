65eb0a4740e396f0b56bfacb61335c7eacbec22d
HysenChan <HysenChan@163.com>
2021-06-14 23:48:21 +0800

    Add chapter03 show base type of bytes && place holder.

---
+++ Solution/Chapter03/Chapter03_main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<climits>
+#include "Chapter03_main.h"
+
+using namespace std;
+
+void sizeofBaseType();//展示int、short、long、long long字节和最大长度
+
+int main()
+{
+	sizeofBaseType();
+	return 0;
+}
+
+void sizeofBaseType()
+{
+	int n_int = INT_MAX;
+	short n_short = SHRT_MAX;
+	long n_long = LONG_MAX;
+	long long n_llong = LLONG_MAX;
+
+	cout << "bytes:" << endl;
+	cout << "int:" << sizeof(int) << endl;
+	cout << "short:" << sizeof(short) << endl;
+	cout << "long:" << sizeof(long) << endl;
+	cout << "long long:" << sizeof(long long) << endl;
+	cout << endl;
+
+	cout << "place holder:" << endl;
+	cout << "int:" << n_int << endl;
+	cout << "short:" << n_short << endl;
+	cout << "long:" << n_long << endl;
+	cout << "long long:" << n_llong << endl;
+}
