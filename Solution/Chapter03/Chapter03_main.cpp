384393e89fa72b5b0949d2ce44b6ae01fe041eea
HysenChan <HysenChan@163.com>
2021-06-18 16:13:11 +0800

    【add】float和double的精度区别

--- Solution/Chapter03/Chapter03_main.cpp
+++ Solution/Chapter03/Chapter03_main.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 #include<climits>
+#include<cfloat>
 #include "Chapter03_main.h"
 
 using namespace std;
 
 int main()
 {
-	sizeofBaseType();
+	//sizeofBaseType();
+	displayFloatAndDouble();
 	return 0;
 }
 
@@ -29,4 +31,18 @@ void sizeofBaseType()
 	cout << "short:" << n_short << endl;
 	cout << "long:" << n_long << endl;
 	cout << "long long:" << n_llong << endl;
+}
+
+void displayFloatAndDouble()
+{
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+	float tub = 10.0 / 3.0;
+	double mint = 10.0 / 3.0;
+	const float million = 1.0e6;
+
+	cout << "tub=" << tub << endl;
+	cout << "tub * million=" << tub * million << endl;
+	cout << "10 * tub * million=" << 10 * tub * million << endl;
+	cout << "mint=" << mint << endl;
+	cout << "mint*million=" << mint * million << endl;
 }
--- Solution/Chapter03/Chapter03_main.h
+++ Solution/Chapter03/Chapter03_main.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void sizeofBaseType();//展示int、short、long、long long字节和最大长度
+void displayFloatAndDouble();//展示float和double
