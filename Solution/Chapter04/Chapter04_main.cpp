358833cb73e402356e5ac1b7daf8054748ac7a2a
HysenChan <HysenChan@163.com>
2021-06-24 15:19:29 +0800

    [change]rebuild code

--- Solution/Chapter04/Chapter04_main.cpp
+++ Solution/Chapter04/Chapter04_main.cpp
@@ -54,11 +54,10 @@ void InStr1Cpp()
 	cout << "Enter your name:\n";
 	//cin >> name;
 	//cin.getline(name, ArSize);
-	cin.get(name, ArSize);
+	cin.get(name, ArSize).get();
 	cout << "Enter your favorite dessert:\n";
 	//cin >> dessert;
 	//cin.getline(dessert, ArSize);
-	cin.get();
 	cin.get(dessert, ArSize);
 	cout << "I have some delicious " << dessert << " for you," << name<<".\n";
 }
