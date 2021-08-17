ae8f6173745a1ce8412dfa1e973a62481d63111b
HysenChan <HysenChan@163.com>
2021-08-17 11:57:58 +0800

    add if base

--- Solution/Chapter06/Chapter06_main.cpp
+++ Solution/Chapter06/Chapter06_main.cpp
@@ -1,6 +1,27 @@
-#include"Chpater06_main.h"
+#include"Chapter06_main.h"
 
 int main()
 {
+	IfBase();
 	return 0;
+}
+
+void IfBase()
+{
+	using namespace std;
+	char ch;
+	int spaces = 0;
+	int total = 0;
+	cin.get(ch);
+	while (ch != '.')
+	{
+		if (ch == ' ')
+		{
+			++spaces;
+		}
+		++total;
+		cin.get(ch);
+	}
+	cout << "Spaces:" << spaces << endl;
+	cout << "Total char:" << total << endl;
 }
