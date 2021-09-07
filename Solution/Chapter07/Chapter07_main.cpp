2da1ee94816bf3d322ba553a0dbe9f23f1af5c80
HysenChan <HysenChan@163.com>
2021-09-07 18:02:15 +0800

    [add]add base function.

--- Solution/Chapter07/Chapter07_main.cpp
+++ Solution/Chapter07/Chapter07_main.cpp
@@ -2,6 +2,20 @@
 
 int main()
 {
-	
+	Calling();
 	return 0;
+}
+
+void Simple()
+{
+	using namespace std;
+	cout << "I'm but a simple function.\n";
+}
+
+void Calling()
+{
+	using namespace std;
+	cout << "Calling() will call the simple() function:\n";
+	Simple();
+	cout << "Finish calling!\n";
 }
