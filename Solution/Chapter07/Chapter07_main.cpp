21681011066216bee5ed463f2515fe5e54f6c0b7
HysenChan <HysenChan@163.com>
2021-09-08 10:22:22 +0800

    [add]protos

--- Solution/Chapter07/Chapter07_main.cpp
+++ Solution/Chapter07/Chapter07_main.cpp
@@ -2,7 +2,8 @@
 
 int main()
 {
-	Calling();
+	protos();
+	//Calling();
 	return 0;
 }
 
@@ -18,4 +19,29 @@ void Calling()
 	cout << "Calling() will call the simple() function:\n";
 	Simple();
 	cout << "Finish calling!\n";
+}
+
+void cheers(int n)
+{
+	using namespace std;
+	for (int i = 0; i < n; i++)
+		cout << "Cheers!";
+	cout << endl;
+}
+
+double cube(double x)
+{
+	return x * x * x;
+}
+
+void protos()
+{
+	using namespace std;
+	cheers(5);
+	cout << "Give me a number:";
+	double side;
+	cin >> side;
+	double volume = cube(side);
+	cout << "A " << side << "-foot cube has a volume of " << volume << " cubic feet.\n";
+	cheers(cube(2));
 }
--- Solution/Chapter07/Chapter07_main.h
+++ Solution/Chapter07/Chapter07_main.h
@@ -2,5 +2,11 @@
 
 #include<iostream>
 
+//Calling
 void Simple();
 void Calling();
+
+//Protos
+void cheers(int);
+double cube(double x);
+void protos();
