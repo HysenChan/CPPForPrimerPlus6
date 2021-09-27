9a0f9e0c7418659f0f245be1c26ca5c3220741ce
HysenChan <HysenChan@163.com>
2021-09-27 14:48:26 +0800

    [add]use typedef

--- Solution/Chapter07/Chapter07_main.cpp
+++ Solution/Chapter07/Chapter07_main.cpp
@@ -545,9 +545,11 @@ const double* f3(const double ar[], int)
 void arfupt()
 {
 	using namespace std;
+	typedef const double* (*p_fun)(const double*, int);
 	double av[3] = { 1112.3,1542.6,2227.9 };
 
-	const double* (*p1)(const double*, int) = f1;
+	p_fun p1 = f1;
+	//const double* (*p1)(const double*, int) = f1;
 	auto p2 = f2;
 
 	cout << "Using pointers to function:\n";
@@ -555,7 +557,8 @@ void arfupt()
 	cout << (*p1)(av, 3) << ":" << *(*p1)(av, 3) << endl;
 	cout << p2(av, 3) << ":" << *p2(av, 3) << endl;
 
-	const double* (*pa[3])(const double*, int) = { f1,f2,f3 };
+	p_fun pa[3] = { f1,f2,f3 };
+	//const double* (*pa[3])(const double*, int) = { f1,f2,f3 };
 	auto pb = pa;
 	cout << "\nmethod 1 of pa: Using an array of pointers to functions:\n";
 	cout << " Address Value\n";
@@ -572,9 +575,11 @@ void arfupt()
 
 	cout << "\npc: Using a pointer to an array of pointers:\n";
 	cout << " Address Value\n";
-	auto pc = &pa;
+	p_fun(*pc)[3] = &pa;
+	//auto pc = &pa;
 
 	cout << (*pc)[0](av, 3) << ":" << *(*pc)[0](av, 3) << endl;
+
 
 	const double* (*(*pd)[3])(const double*, int) = &pa;
 	const double* pdb = (*pd)[1](av, 3);
