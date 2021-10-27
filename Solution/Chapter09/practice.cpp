367aa451fb2a5adab3da59e2c73e5d3fc8584c27
HysenChan <HysenChan@163.com>
2021-10-27 17:31:40 +0800

    [add]practice06

--- Solution/Chapter09/practice.cpp
+++ Solution/Chapter09/practice.cpp
@@ -3,7 +3,8 @@
 
 int main()
 {
-	Practice01();
+	Practice02();
+	//Practice01();
 	return 0;
 }
 
@@ -25,4 +26,43 @@ void Practice01()
 		std::cout << "Next golf:\n";
 		i++;
 	}
+}
+
+void Practice02()
+{
+	using namespace std;
+	string input;
+	char next;
+
+	cout << "Enter a line:\n";
+	getline(cin, input);
+	if (input == "")
+	{
+		std::cout << "Bye\n";
+		return;
+	}
+	while (cin)
+	{
+		strCount(input);
+		cout << "Enter next line (empty line to quit):\n";
+		getline(cin, input);
+		if (input == "")
+		{
+			std::cout << "Bye\n";
+			return;
+		}
+	}
+}
+
+void strCount(const std::string str)
+{
+	static int total = 0;
+	int count = 0;
+
+	std::cout << "\"" << str << "\" contains ";
+	while (str[count])
+		count++;
+	total += count;
+	std::cout << count << " character\n";
+	std::cout << total << " character total\n";
 }
--- Solution/Chapter09/practice.h
+++ Solution/Chapter09/practice.h
@@ -1,3 +1,6 @@
 #pragma once
+#include <string>
 
 void Practice01();
+void Practice02();
+void strCount(const std::string str);
