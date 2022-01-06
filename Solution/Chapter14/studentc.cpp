467c56509615edb3057ff6b879b7cc527bc9cba1
HysenChan <HysenChan@163.com>
2022-01-06 14:44:18 +0800

    [add]改成私有继承

--- Solution/Chapter14/studentc.cpp
+++ Solution/Chapter14/studentc.cpp
@@ -7,12 +7,12 @@ Student::~Student()
 std::ostream& Student::arr_out(std::ostream& os) const
 {
 	int i;
-	int lim = scores.size();
+	int lim = ArrayDb::size();
 	if (lim > 0)
 	{
 		for (i = 0; i < lim; i++)
 		{
-			os << scores[i] << " ";
+			os << ArrayDb::operator[](i) << " ";
 			if (i % 5 == 4)
 				os << std::endl;
 		}
@@ -26,42 +26,42 @@ std::ostream& Student::arr_out(std::ostream& os) const
 
 double Student::Average() const
 {
-	if (scores.size() > 0)
-		return scores.sum() / scores.size();
+	if (ArrayDb::size() > 0)
+		return ArrayDb::sum() / ArrayDb::size();
 	else
 		return 0;
 }
 
 const std::string& Student::Name() const
 {
-	return name;
+	return (const std::string&)*this;
 }
 
 double& Student::operator[](int i)
 {
-	return scores[i];
+	return ArrayDb::operator[](i);
 }
 
 double Student::operator[](int i) const
 {
-	return scores[i];
+	return ArrayDb::operator[](i);
 }
 
 std::istream& operator>>(std::istream& is, Student& stu)
 {
-	is >> stu.name;
+	is >> (std::string&)stu;
 	return is;
 }
 
 std::istream& getline(std::istream& is, Student& stu)
 {
-	getline(is, stu.name);
+	getline(is, (std::string&)stu);
 	return is;
 }
 
 std::ostream& operator<<(std::ostream& os, const Student& stu)
 {
-	os << "Scores for " << stu.name << ":\n";
+	os << "Scores for " << (const std::string&)stu << ":\n";
 	stu.arr_out(os);
 	return os;
 }
--- Solution/Chapter14/studentc.h
+++ Solution/Chapter14/studentc.h
@@ -4,21 +4,19 @@
 #include<string>
 #include<valarray>
 
-class Student
+class Student :private std::string, private std::valarray<double>
 {
 private:
 	typedef std::valarray<double> ArrayDb;
-	std::string name;
-	ArrayDb scores;
 	std::ostream& arr_out(std::ostream& os)const;
 
 public:
-	Student() :name("Null Student"), scores() {};
-	explicit Student(const std::string& s) :name(s), scores() {}
-	explicit Student(int n) :name("Nully"), scores(n) {}
-	Student(const std::string& s, int n) :name(s), scores(n) { }
-	Student(const std::string& s, const ArrayDb& a) :name(s), scores(a) { }
-	Student(const char* str, const double* pd, int n) :name(str), scores(pd, n) {}
+	Student() :std::string("Null Student"), ArrayDb() {};
+	explicit Student(const std::string& s) :std::string(s), ArrayDb() {}
+	explicit Student(int n) :std::string("Nully"), ArrayDb(n) {}
+	Student(const std::string& s, int n) :std::string(s), ArrayDb(n) { }
+	Student(const std::string& s, const ArrayDb& a) :std::string(s), ArrayDb(a) { }
+	Student(const char* str, const double* pd, int n) :std::string(str), ArrayDb(pd, n) {}
 	~Student();
 	double Average()const;
 	const std::string& Name()const;
