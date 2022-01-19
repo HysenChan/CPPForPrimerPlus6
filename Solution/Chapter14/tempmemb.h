b07c7c0879caed92ed93579141d9378fc43fb984
HysenChan <HysenChan@163.com>
2022-01-19 15:19:14 +0800

    [add]多重继承

--- Solution/Chapter14/tempmemb.h
+++ Solution/Chapter14/tempmemb.h
@@ -7,21 +7,32 @@ class beta
 public:
 	beta(T t, int i) :q(t), n(i) {};
 	template<typename U>
-	U blab(U u, T t) { return (n.Value() + q.Value()) * u / t; }
+	U blab(U u, T t);
 	void Show()const { q.show(); n.show(); }
 
 private:
 	template<typename V>
-	class hold
-	{
-	public:
-		hold(V v = 0) :val(v) {};
-		void show()const { cout << val << endl; }
-		V Value()const { return val; }
-
-	private:
-		V val;
-	};
+	class hold;
 	hold<T> q;
 	hold<int> n;
 };
+
+template <typename T>
+template<typename V>
+class beta<T>::hold
+{
+public:
+	hold(V v = 0) :val(v) {};
+	void show()const { cout << val << endl; }
+	V Value()const { return val; }
+
+private:
+	V val;
+};
+
+template<typename T>
+template<typename U>
+U beta<T>::blab(U u, T t)
+{
+	return (n.Value() + q.Value()) * u / t;
+}
