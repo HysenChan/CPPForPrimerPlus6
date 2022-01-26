2dd6f09983b0f48ad391cb81b7f1b74090020d19
HysenChan <HysenChan@163.com>
2022-01-26 16:53:16 +0800

    [add]

--- Solution/Chapter15/chapter15_main.cpp
+++ Solution/Chapter15/chapter15_main.cpp
@@ -7,9 +7,15 @@ void use_tv();
 
 void error1();
 
+double hmean(double a, double b);
+double gmean(double a, double b);
+
+void error5();
+
 int main()
 {
-	error1();
+	error5();
+	//error1();
 	//use_tv();
 	return 0;
 }
@@ -48,13 +54,72 @@ void error1()
 	std::cout << "Enter two numbers:";
 	while (std::cin >> x >> y)
 	{
+		//error1
 		/*z = hmean(x, y);
 		std::cout << "Harmonic mean of " << x << " and " << y << " is " << z << std::endl;*/
-		if (hmean(x, y, &z))
+
+		//error2
+		/*if (hmean(x, y, &z))
 			std::cout << "Harmonic mean of " << x << " and " << y << " is " << z << std::endl;
 		else
-			std::cout << "One value should not be the negative of the other - try again.\n";
-		std::cout << "Enter next set of numbers < q to quit >:";
+			std::cout << "One value should not be the negative of the other - try again.\n";*/
+
+			//error3
+		try
+		{
+			z = hmean(x, y);
+			std::cout << "Harmonic mean of " << x << " and " << y << " is " << z << std::endl;
+			std::cout << "Geometric mean of " << x << " and " << y << " is " << gmean(x, y) << std::endl;
+		}
+		catch (bad_hmean& bg)
+		{
+			bg.mesg();
+			cout << "Try again.\n";
+			continue;
+		}
+		catch (bad_gmean& hg)
+		{
+			cout << hg.mesg();
+			cout << "values used:" << hg.v1 << "," << hg.v2 << endl;
+			cout << "Sorry, you don't get to play any more.\n";
+			break;
+		}
+		/*std::cout << "Harmonic mean of " << x << " and " << y << " is " << z << std::endl;
+		std::cout << "Enter next set of numbers < q to quit >:";*/
 	}
 	std::cout << "Bye!\n";
+}
+
+inline void error5()
+{
+	double x, y, z;
+	{
+		demo d1("found in block in main()");
+		cout << "Enter two numbers:";
+		while (cin >> x >> y)
+		{
+			try
+			{
+				z = means(x, y);
+				cout << "The mean mean of " << x << " and " << y << " is " << z << endl;
+			}
+			catch (bad_hmean& bg)
+			{
+				bg.mesg();
+				cout << "Try again.\n";
+				continue;
+			}
+			catch (bad_gmean& hg)
+			{
+				cout << hg.mesg();
+				cout << "Values used:" << hg.v1 << "," << hg.v2 << endl;
+				cout << "Sorry,you don't get to play any more.\n";
+				break;
+			}
+		}
+		d1.show();
+	}
+	cout << "Bye!\n";
+	cin.get();
+	cin.get();
 }
--- Solution/Chapter15/error1.h
+++ Solution/Chapter15/error1.h
@@ -2,6 +2,36 @@
 #include<iostream>
 #include<cstdlib>
 
+class bad_hmean
+{
+public:
+	bad_hmean(int a = 0, int b = 0) :v1(a), v2(b) {};
+	void mesg();
+
+private:
+	double v1;
+	double v2;
+};
+
+inline void bad_hmean::mesg()
+{
+	std::cout << "hmean(" << v1 << "," << v2 << "):" << "invalid arguments:a = -b \n";
+}
+
+class bad_gmean
+{
+public:
+	double v1;
+	double v2;
+	bad_gmean(int a = 0, int b = 0) :v1(a), v2(b) {};
+	const char* mesg();
+};
+
+inline const char* bad_gmean::mesg()
+{
+	return "gmean() arguments should be >= 0\n";
+}
+
 double hmean(double a, double b);
 
 bool hmean(double a, double b, double* ans);
@@ -10,10 +40,41 @@ inline double hmean(double a, double b)
 {
 	if (a == -b)
 	{
-		std::cout << "untenable arguments to hmean()\n";
-		std::abort();
+		//error1
+		/*std::cout << "untenable arguments to hmean()\n";
+		std::abort();*/
+
+		//error2
+		throw bad_hmean(a, b);
 	}
 	return 2.0 * a * b / (a + b);
+}
+
+inline double gmean(double a, double b)
+{
+	if (a < 0 || b < 0)
+		throw bad_gmean(a, b);
+	return std::sqrt(a * b);
+}
+
+inline double means(double a, double b)
+{
+	double am, hm, gm;
+	demo d2("found in means()");
+	am = (a + b) / 2.0;
+	try
+	{
+		hm = hmean(a, b);
+		gm = gmean(a, b);
+	}
+	catch (bad_hmean& bg)
+	{
+		bg.mesg();
+		std::cout << "Caught in means()\n";
+		throw;
+	}
+	d2.show();
+	return (am + hm + gm) / 3.0;
 }
 
 inline bool hmean(double a, double b, double* ans)
@@ -29,3 +90,28 @@ inline bool hmean(double a, double b, double* ans)
 		return true;
 	}
 }
+
+class demo
+{
+public:
+	demo(const std::string& str)
+	{
+		word = str;
+		std::cout << "demo " << word << " created\n";
+	}
+	~demo()
+	{
+		std::cout << "demo " << word << " destroyed\n";
+	}
+	void show()const
+	{
+		std::cout << "demo " << word << " lives!\n";
+	}
+
+private:
+	std::string word;
+};
+
+double hmean(double a, double b);
+double gmean(double a, double b);
+double means(double a, double b);
